e9a0f114cb42387950d2cb5a2a206a0a85fdbea2
TrinhVR <trinh.simpson@lethpolytech.ca>
2024-09-23 13:05:20 -0600

    Monobehaviour Folder Created

--- FL24VXR_Trinh/Assets/VXR1170/1170_Scripts/Monobehaviour Scripts/TransformComponents.cs
+++ FL24VXR_Trinh/Assets/VXR1170/1170_Scripts/Monobehaviour Scripts/TransformComponents.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonoExamples : MonoBehaviour
+public class TransformComponents : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
